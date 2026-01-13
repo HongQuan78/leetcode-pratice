4f244caae30f281ddf2ad977fb590367a0f51f93
HongQuan78 <vhquan313@gmail.com>
2026-01-13 16:56:51 +0700

    Add Separate Squares I solution

---
+++ Separate Squares I/solution.cs
@@ -0,0 +1,38 @@
+public class Solution {
+    public double SeparateSquares(int[][] squares) {
+        double total = 0;
+        double low = 1e9;
+        double high = 0;
+
+        foreach(var s in squares)
+        {
+            total += (double) s[2] *s[2];
+            low = Math.Min(low, s[1]);
+            high = Math.Max(high, s[1] + s[2]);
+        }
+
+        double half = total / 2;
+        double eps = 1e-5;
+        while (Math.Abs(high - low) > eps)
+        {
+            double mid = (low + high) / 2;
+            double below = 0;
+
+            foreach(var s in squares)
+            {
+                double y = s[1];
+                double l = s[2];
+                below += Math.Max(0, Math.Min(l, mid - y)) * l;
+            }
+            if(below >= half)
+            {
+                high = mid;
+            }
+            else
+            {
+                low = mid;
+            }
+        }
+        return low;
+    }
+}
